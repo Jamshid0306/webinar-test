b6ea9b5e7582ec15fbf66c75f5553f7650010cae
Jamshid <jamshidabdukarimov776@gmail.com>
2025-08-14 21:23:00 +0500

    add ngrok

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true,
+    port: 5173,
+    allowedHosts: [
+      '3f9bedee2738.ngrok-free.app',
+      'localhost'
+    ]
+  }
 });
